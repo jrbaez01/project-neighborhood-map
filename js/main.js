b42406dbf7a7e71ff125107b9d97a6fbce546a11
Junior Báez <juniorbaez01@gmail.com>
2017-10-26 15:26:16 -0400

    Use fetch to get locations. Use click and css binding to toggle menu. Applied code review feedbacks.

--- js/main.js
+++ js/main.js
@@ -3,63 +3,12 @@
 	/**
 	 * DOM elements
 	 */
-	const _burger = document.querySelector('.navbar-burger'),
-		  _menu = document.getElementById(_burger.dataset.target),
-		  _map = document.getElementById('map'),
-		  _markersFilterInput = document.getElementById('markersFilterInput'),
-		  _markersFilterDropdown = document.getElementById('markersFilterDropdown');
-
-	/**
-	 * Location Data
-	 */
-	const _newyork_city  = {lat: 40.734389, lng: -73.993212};
-	const _newyork_locations = [
-		{
-				name: 'Lower Manhattan',
-				position: {
-						lat: 40.712429,
-						lng: -74.007803
-				},
-				info: ''
-		},
-		{
-				name: 'Central Park',
-				position: {
-						lat: 40.771178,
-						lng: -73.974239
-				},
-				info: ''
-		},
-		{
-				name: 'Statue of Liberty National Monument',
-				position: {
-						lat: 40.689247,
-						lng: -74.044471
-				},
-				info: ''
-		},
-		{
-				name: 'Brooklyn Bridge',
-				position: {
-						lat: 40.706072,
-						lng: -73.996894
-				},
-				info: ''
-		},
-		{
-				name: 'Manhattan Bridge',
-				position: {
-						lat: 40.707571,
-						lng: -73.990727
-				},
-				info: ''
-		}
-	];
+	const MAP_CONTAINER = document.getElementById('map');
 
 	/**
 	 * Google map and info window containers
 	 */
-	let _googleMap, _infoWindow;
+	let googleMap, infoWindow;
 
 	/**
 	 * Marker constructor
@@ -68,31 +17,31 @@
 		let self = this;
 
 		// Marker raw location data
-		self._location = location;
+		self.location = location;
 
 		// The actual marker
-		self._marker = new google.maps.Marker({
+		self.marker = new google.maps.Marker({
 			position: location.position,
 			title: location.name,
-			map: _googleMap
+			map: googleMap
 		});
 
 		// True or False, depending if the marker match the filter
-		self._matchMarkersFilter = ko.computed(function() {
-			let isMatch = self._location.name.toLowerCase()
+		self.matchMarkersFilter = ko.computed(function() {
+			let isMatch = self.location.name.toLowerCase()
 				.indexOf(
 					filter().toLowerCase()
 				) >= 0;
 
-			self._marker.setMap( isMatch ? _googleMap : null );
+			self.marker.setVisible( isMatch );
 			return isMatch;
 		});
 
 		// Animate marker
 		self.bounce = function(time=1000) {
-			self._marker.setAnimation(google.maps.Animation.BOUNCE);
+			self.marker.setAnimation(google.maps.Animation.BOUNCE);
 			setTimeout(function() {
-				self._marker.setAnimation(null);
+				self.marker.setAnimation(null);
 			}, time);
 		};
 
@@ -100,8 +49,8 @@
 		self.loadInfoWindow = function() {
 			// Center map and animate marker
 			self.bounce();
-			_googleMap.panTo(self._marker.getPosition());
-			_googleMap.setZoom(15);
+			googleMap.panTo(self.marker.getPosition());
+			googleMap.setZoom(15);
 
 			// Make ajax request
 			$.ajax( "http://en.wikipedia.org/w/api.php", {
@@ -111,14 +60,14 @@
 				data: {
 					action: "opensearch",
 					format: "json",
-					search: self._location.name,
+					search: self.location.name,
 					limit: 10,
 					profile: "fuzzy"
 				}
 			})
 			.done(function(response) {
 				// If success, format info and store
-				self._location.info =
+				self.location.info =
 					'<div class="content">' +
 						'<h2>' + response[1][0] + '</h2>' +
 						'<p>' + response[2][0] + '</p>' +
@@ -127,7 +76,7 @@
 			})
 			.fail(function(jqXHR) {
 				// if failed, store error message instead
-				self._location.info =
+				self.location.info =
 					'<div class="content">' +
 						'<p>Failed to load this location info data.' +
 						' Status: ' + jqXHR.status + ' - ' + jqXHR.statusText + '</p>' +
@@ -135,13 +84,13 @@
 			})
 			.always(function() {
 				// Show location info, either it was a success or fail
-				_infoWindow.setContent(self._location.info);
-		        _infoWindow.open(map, self._marker);
+				infoWindow.setContent(self.location.info);
+		        infoWindow.open(map, self.marker);
 			});
 		};
 
 		// Execute loadInfoWindow on click event
-		self._marker.addListener('click', self.loadInfoWindow);
+		self.marker.addListener('click', self.loadInfoWindow);
 	}
 
 	/**
@@ -149,18 +98,41 @@
 	 */
 	function MarkersViewModel() {
 		let self = this;
-		self._markers = ko.observableArray([]);
-		self._markersFilter = ko.observable("");
+		self.markers = ko.observableArray([]);
+		self.markersFilter = ko.observable("");
+		self.markersMenu = ko.observable(true);
 
-		// Create markers
-		_newyork_locations.forEach(function(loc) {
-			self._markers.push(new Marker(loc, self._markersFilter));
+		// Get json locations data with fetch
+		fetch(
+			"https://raw.githubusercontent.com/jrbaez01/project-neighborhood-map/master/locationsdata.json",
+			{
+				mode: 'corn'
+			}
+		)
+		.then(function(response) {
+			// convert response to json
+			return response.json();
+		})
+		.then(function (locations) {
+			// If success, create markers
+			locations.forEach(function(loc) {
+				self.markers.push(new Marker(loc, self.markersFilter));
+			});
+		})
+		.catch(function(error) {
+			// if failed, alert
+			alert("Sorry, failed to load locations data and create markers. " + error);
 		});
 
 		// Show info window for a given marker
 		self.showMarkerInfo = function(marker) {
 			marker.loadInfoWindow();
-			$(_map).click();
+			$(MAP_CONTAINER).click(); // to hide menu
+		};
+
+		// Toggle menu
+		self.markersMenuToggle = function() {
+			self.markersMenu(!self.markersMenu());
 		};
 	}
 
@@ -171,39 +143,39 @@
 		/**
 		 * Create Google map
 		 */
-		_googleMap = new google.maps.Map(_map, {
-			center: _newyork_city ,
+		googleMap = new google.maps.Map(MAP_CONTAINER, {
+			center: {lat: 40.734389, lng: -73.993212}, // TODO: move to json file
 			zoom: 13
 		});
 
 		/**
 		 * Create Google map Info Window
 		 */
-		_infoWindow = new google.maps.InfoWindow();
+		infoWindow = new google.maps.InfoWindow();
 
 		/**
 		 * Create markers and activates knockout.js
 		 */
-		ko.applyBindings(new MarkersViewModel());
+		let markersViewModel = new MarkersViewModel();
+		ko.applyBindings(markersViewModel);
 
 		/**
 		 * Add events,
-		 * Open/close menu when burger is clicked, hide when map is clicked.
+		 * Close menu when map or menu item is clicked.
 		 */
-		$(_burger).on('click', function() {
-			_menu.classList.toggle('is-active');
-		});
-
-		$(_markersFilterInput).on('click', function() {
-			_markersFilterDropdown.classList.add('is-active');
-		});
-
-		$(_map).on('click touchstart', function() {
-			_menu.classList.remove('is-active');
-			_markersFilterDropdown.classList.remove('is-active');
+		$(MAP_CONTAINER).on('click touchstart', function() {
+			markersViewModel.markersMenu(false);
 		});
 	}
 
-	// Export init function to use as google map callback
+	/**
+	 * Handle map loading error
+	 */
+	function handleError() {
+		alert("Sorry, the map could not be loaded. Please refresh and try again.");
+	}
+
+	// Export functions to use as google map callback
 	window.initMap = init;
+	window.handleErrorLoadingMap = handleError;
 })();
